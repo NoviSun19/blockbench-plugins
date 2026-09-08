a0781e273bc554dfca5791451ccb0c73874eaa41
NoviSun19 <novisun85@gmail.com>
2026-09-08 18:45:02 +0900

    Add min_version requirement to model_region_tools

--- plugins/model_region_tools/model_region_tools.js
+++ plugins/model_region_tools/model_region_tools.js
@@ -4,6 +4,7 @@ Plugin.register('model_region_tools', {
     description: 'Tools for selecting regions directly on the 3D model.',
     icon: 'select_all',
     version: '1.0.0',
+    min_version: '4.8.0',
     variant: 'both',
 
     onload() {
